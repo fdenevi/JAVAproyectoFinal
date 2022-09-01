2d72e61e7bb75c6fbbe4de897ddc2c3ed25800c3
Florencia Denevi <fdenevi@hotmail.com>
2022-09-01 19:13:37 -0300

    agrego alerta si no se completa el nombre en formulario

--- js/productos.js
+++ js/productos.js
@@ -291,12 +291,20 @@ function printCarrito() {
 
   const btnFinalizarCompra = document.querySelector(".comprar")
   btnFinalizarCompra.addEventListener("click", () => {
-    Swal.fire({
-      title: 'Compra realizada con éxito ;) !',
-      text: `Gracias por confiar en nosotrxs ${document.querySelector("#name").value}. En los próximos minutos recibirás un correo electrónico con el detalle de tu compra`,
-      imageUrl: 'img/logo_bg.png',
-      imageAlt: 'Custom image',
-    });
+    if (`${document.querySelector("#name").value}` === "") {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Debe ingresar su nombre',
+      });
+    } else {
+      Swal.fire({
+        title: 'Compra realizada con éxito ;) !',
+        text: `Gracias por confiar en nosotrxs ${document.querySelector("#name").value}. En los próximos minutos recibirás un correo electrónico con el detalle de tu compra`,
+        imageUrl: 'img/logo_bg.png',
+        imageAlt: 'Custom image',
+      });
+    }
   })
 }
 printCarrito()
